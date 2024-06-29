4a2290896346ffc97144bb05ec29bd75a30123d5
manoj <pitlamanoj1@gmail.com>
2024-06-30 02:00:58 +0530

    Theme and product page added

---
+++ src/Pages/CategoryForm.jsx
@@ -0,0 +1,77 @@
+
+import React, { useState } from 'react'
+import Input from '../Components/Input'
+
+function CategoryForm() {
+  const [productType, setProductType] = useState("")
+  const HandleProductType = (event) => {
+    setProductType(event.target.value)
+  } 
+  return (
+    <div className="bg-gray-300 flex items-center justify-center min-h-screen">
+      <div className=" relative my-3 h-[90vh] bg-white bg-opacity-80 backdrop-blur-sm shadow-lg rounded-lg p-3">
+
+        <div className=" mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl py-1 sm:py-2 lg:max-w-none lg:py-3">
+            <h2 className="text-2xl font-bold text-blue-500 mb-4">Let's Customize Your Shopnix Store</h2>
+          </div>
+          <div className='flex flex-wrap'>
+          <form className='flex flex-col font-bold mr-4'>
+            <h2 className="w-[20.5em] px-3 bg-blue-200 rounded-lg p-1 text-xl font-bold text-gray-900 mb-2 mt-2">Add a Type, Category and Sub-Category</h2>
+            <Input
+              className="font-normal mb-4"
+              label=" Product type *"
+              type="text"
+              value={productType}
+              onChange={HandleProductType}
+              placeholder="e.g: Electronics"
+            />
+
+            <Input
+              className="font-normal mb-4"
+              label=" Category (optional)"
+              type="text"
+              placeholder="e.g: Mobiles"
+            />
+
+            <Input
+              className="font-normal mb-4"
+              label=" Sub-Category (optional)"
+              type="text"
+              placeholder="e.g: Iphone"
+            />
+          </form>
+
+        <div className='flex flex-col ml-4 p-4 bg-blue-200 font-bold rounded-xl'>
+          <p className='border border-gray-600 bg-orange-500 rounded-lg h-14 w-36 text-center flex items-center justify-center'>Product type</p>
+          <div className='flex justify-center items-center w-36 '>
+              <span className=' text-7xl'>&#x2193;</span>
+            </div>
+
+          <p className='border border-gray-600 bg-orange-500 rounded-lg h-14 w-36 text-center flex items-center justify-center'> Category</p>
+          <div className=' flex justify-center items-center w-36'>
+              <span className='text-7xl'>&#x2193;</span>
+            </div>
+          <div className='flex flex-row'>
+            <p className='border border-gray-600 bg-orange-500 rounded-lg h-14 w-36 text-center flex items-center justify-center'>Sub-Category </p>
+            
+            <div className='flex justify-center items-center h-10'>
+              <span className='text-7xl'>&#x2192;</span>
+            </div>
+            <p className='border border-gray-600 bg-blue-500 rounded-lg h-14 w-36 text-center flex items-center justify-center'>Product</p>
+          </div>
+        </div>
+        </div>
+        </div>
+        <div className='absolute bottom-1 left-96 '>
+          <button className='button mx-2 '>Back</button>
+          <button className={`button mx-2 ${productType === "" ? 'disabled:opacity-50 text-gray-400 bg-gray-300 cursor-not-allowed' : 'bg-blue-500 text-white'}`}
+          disabled={productType === ""}>Next</button>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CategoryForm
---
+++ src/Pages/ThemeCard.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import '../App.css'
+function ThemeCard() {
+  // let [theme, setTheme] = useState('')
+  // console.log(theme)
+  const callouts = [
+    {
+      name: 'Shoes theme',
+
+      imageSrc: 'https://cdn.dribbble.com/userupload/5832070/file/original-41f45ae148747834799c2c0586bc2d05.jpg?resize=1504x1128',
+      imageAlt: 'Shoes Theme ',
+      href: '#',
+    },
+    {
+      name: 'Electronics Theme',
+      imageSrc: 'https://cdn.dribbble.com/users/3531878/screenshots/18950054/media/ad158ef3af9ba2fe847399101a4d4299.jpg?resize=1200x900&vertical=center',
+      imageAlt: 'Electronics Theme',
+      href: '#',
+    },
+    {
+      name: 'Furniture theme',
+      imageSrc: 'https://cdn.dribbble.com/userupload/12732084/file/original-8f5429840faf7816cba6609f01225162.jpg?resize=1504x1222',
+      imageAlt: 'Furniture Theme',
+      href: '#',
+    },
+  ]
+
+
+  const [selectedThemeIndex, setSelectedThemeIndex] = useState(null);
+
+  const handleClick = (index) => {
+    setSelectedThemeIndex(index === selectedThemeIndex ? null : index);
+  };
+
+
+  return (
+    <div className="bg-gray-300 flex items-center justify-center min-h-screen">
+      <div className="relative w-[70rem] my-3 bg-white bg-opacity-80 backdrop-blur-sm shadow-lg rounded-lg p-3">
+
+        <div className="">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl py-1 sm:py-2 lg:max-w-none lg:py-3">
+              <h2 className="text-2xl font-bold text-gray-900 mb-2">Let's Customize Your Shopnix Store</h2>
+              <h2 className="text-xl font-bold text-gray-900 mt-2 mb-2">Apply a theme</h2>
+
+              <div className="mt-1 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+                {callouts.map((callout, index) => (
+                  <div key={callout.name} className="group relative rounded-xl p-0.5 border-gray-300 border-2">
+                    <div className="relative w-full rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                      <img
+                        src={callout.imageSrc}
+                        alt={callout.imageAlt}
+                        className="w-full h-56 object-cover object-center"
+                      />
+                    </div>
+                    <h3 className="mb-4 text-lg text-center font-bold text-gray-900">
+                      {callout.name}
+                    </h3>
+                    <div className='flex justify-center items-center mt-1 mb-3'>
+                      <button
+                        onClick={() => handleClick(index)}
+                        className={`button ${selectedThemeIndex === index
+                          ? ' bg-blue-500 text-white'
+                          : ''
+                          }`}
+                      >
+                        {selectedThemeIndex === index ? 'Applied' : 'Choose'}
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <p className='text-gray-500 my-2'>Note: You can explore more themes from the admin panel</p>
+              <div className='flex justify-end'>
+                <button
+                  className={`button bg-blue-500 text-white font-semibold py-1.5 px-4 rounded-lg focus:outline-none focus:shadow-outline ${selectedThemeIndex ? '' : 'opacity-50 cursor-not-allowed'}`}
+                  disabled={!selectedThemeIndex}
+                >
+                  Next
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  )
+}
+
+export default ThemeCard
